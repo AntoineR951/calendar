c0a7f393898f69e8dc84cf7899899142a934355b
AntoineR951 <32392277+AntoineR951@users.noreply.github.com>
2026-01-29 00:56:00 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     base: './', // CRUCIAL pour que le site marche dans n'importe quel dossier sur OVH
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
-    }
+    plugins: [react()]
   };
 });
